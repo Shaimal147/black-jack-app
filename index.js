6258121548b1d1ef6c5fae483eb717f3259bf2c3
Shaimal <shaimalabdulla35@gmail.com>
2026-02-05 20:24:38 +0500

    Refactor code. Introduce helper functions render() calculateSum()

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function game() {
-    let cards = [11, 10]
-    let sum = cards.reduce((total, current) => {
-        return  total + current
-    }, 0)
+    let cards = [2, 3]
+    let sum = calculateSum()
     let hadBlackjack = false
     let isAlive = true
     let message = ""
@@ -11,31 +9,51 @@ function game() {
     const messageEl = document.getElementById("message-el")
     const sumEl = document.getElementById("sum-el")
     const cardsEl = document.getElementById("cards-el")
+    const newCardBtn = document.getElementById("new-card-btn")
 
     startBtn.addEventListener("click", startGame)
+    newCardBtn.addEventListener("click", newCardButton)
 
     function startGame() {
-        cardsEl.textContent = `Cards: ${cards.join(" - ")}`
-        gameRules()
-        sumEl.textContent = `Sum: ${sum}`
+        newCardBtn.hidden = false
+        render()
     }
 
     function gameRules() {
         if (sum <= 20) {
             message = "Do you want to draw a new card?"
-            messageEl.textContent = message
         } else if (sum === 21) {
             message = "Whooo! You've got Blackjack!"
             hadBlackjack = true
-            messageEl.textContent = message
+            newCardBtn.hidden = true
         } else {
             message = "You're out of the game!"
             isAlive = false
-            messageEl.textContent = message
+            newCardBtn.hidden = true
         }
+
+        messageEl.textContent = message
     }
+
+    function newCardButton() {
+        if (!isAlive || hadBlackjack) return
+        cards.push(1)
+        render()
+    }
+
+    function calculateSum() {
+        return cards.reduce((total, current) => total + current, 0)
+    }
+
+    function render() {
+        sum = calculateSum()
+        cardsEl.textContent = `Cards: ${cards.join(" - ")}`
+        sumEl.textContent = `Sum: ${sum}`
+        gameRules()
+    }
+
 }
 
 
 
-const blackJack = game()
+game()
