481648066c866e73d66e262b442154d09950b44c
Shaimal <shaimalabdulla35@gmail.com>
2026-02-05 19:39:50 +0500

    Add card dealing + display results

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 function game() {
-    let firstCard = 17
-    let secondCard = 4
-    let sum = firstCard + secondCard
+    let cards = [11, 10]
+    let sum = cards.reduce((total, current) => {
+        return  total + current
+    }, 0)
     let hadBlackjack = false
     let isAlive = true
     let message = ""
 
     const startBtn = document.getElementById("start-btn")
+    const messageEl = document.getElementById("message-el")
+    const sumEl = document.getElementById("sum-el")
+    const cardsEl = document.getElementById("cards-el")
 
     startBtn.addEventListener("click", startGame)
 
     function startGame() {
+        cardsEl.textContent = `Cards: ${cards.join(" - ")}`
         gameRules()
+        sumEl.textContent = `Sum: ${sum}`
     }
 
     function gameRules() {
         if (sum <= 20) {
             message = "Do you want to draw a new card?"
-            console.log(message)
+            messageEl.textContent = message
         } else if (sum === 21) {
             message = "Whooo! You've got Blackjack!"
             hadBlackjack = true
-            console.log(message)
+            messageEl.textContent = message
         } else {
             message = "You're out of the game!"
             isAlive = false
-            console.log(message)
+            messageEl.textContent = message
         }
     }
 }
